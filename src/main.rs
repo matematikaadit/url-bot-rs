bfeb2959417c249d083ab7173f905bcd4cfff202
Edward Cragg <drq.11235@gmail.com>
2018-07-30 09:16:50 +0100

    wip: logging: try box

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,8 @@ struct LogEntry {
     time_created: Timespec,
 }
 
-fn add_log(db: &Connection, e: &LogEntry) {
-	db.execute("INSERT INTO posts (url, user, channel, time_created)
+fn add_log(db: Box<Option<Connection>>, e: &LogEntry) {
+	*db.execute("INSERT INTO posts (url, user, channel, time_created)
 		VALUES (?1, ?2, ?3, ?4)",
 		&[&e.url, &e.user, &e.channel, &e.time_created]).unwrap();
 }
@@ -64,7 +64,7 @@ fn main() {
         	.and_then(|d| d.deserialize())
         	.unwrap_or_else(|e| e.exit());
 
-	let db;
+	let mut db: Box<Option<Connection>>;
 	if !args.db.is_empty()
 	{
 		/* open the sqlite database for logging */
@@ -72,7 +72,7 @@ fn main() {
 		/* TODO: make logging optional */
 		println!("Using database at: {}", args.db);
 
-		db = Some(Connection::open(args.db).unwrap());
+		db = Box::new(Some(Connection::open(args.db).unwrap()));
 		db.unwrap().execute("CREATE TABLE IF NOT EXISTS posts (
 			id		INTEGER PRIMARY KEY,
 			url		TEXT NOT NULL,
@@ -81,7 +81,7 @@ fn main() {
 			time_created	TEXT NOT NULL
 			)", &[]).unwrap();
 	} else {
-		db = None;
+		db = Box::new(None);
 	}
 
 	let server = IrcServer::new("config.toml").unwrap();
@@ -122,9 +122,8 @@ fn main() {
 								channel: target.to_string(),
 								time_created: time::get_time(),
 							};
-							match db {
-								Some(d) => {add_log(&d, &entry);}
-								None	=> ()
+							if !args.db.is_empty() {
+								add_log(db, &entry);
 							}
 						}
 						_ => ()
